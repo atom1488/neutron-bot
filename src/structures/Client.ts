07e4fce5728da152ce60a9bc999c222bafd4e2e4
atomkern <atom1488@gpa.lu>
2022-06-16 05:47:21 +0200

    bot handler

---
+++ src/structures/Client.ts
@@ -0,0 +1,79 @@
+import {
+    ApplicationCommandDataResolvable,
+    Client,
+    ClientEvents,
+    Collection
+} from "discord.js";
+import { CommandType } from "../typings/Command";
+import glob from "glob";
+import { promisify } from "util";
+import { RegisterCommandsOptions } from "../typings/client";
+import { Event } from "./Event";
+import path from 'path';
+
+const globPromise = promisify(glob);
+
+export class ExtendedClient extends Client {
+    commands: Collection<string, CommandType> = new Collection();
+
+    constructor() {
+        super({ intents: 32767 });
+    }
+
+    start() {
+        console.log("Bot is starting...");
+        this.registerModules();
+        this.login(process.env.botToken);
+    }
+    async importFile(filePath: string) {
+        return (await import(filePath))?.default;
+    }
+
+    async registerCommands({ commands, guildId }: RegisterCommandsOptions) {
+        if (guildId) {
+            this.guilds.cache.get(guildId)?.commands.set(commands);
+            console.log(`Registering commands to ${guildId}`);
+        } else {
+            this.application?.commands.set(commands);
+            console.log("Registering global commands");
+        }
+    }
+
+    async registerModules() {
+        // Commands
+        const slashCommands: ApplicationCommandDataResolvable[] = [];
+        const directory = path.join(__dirname, '../commands/info');
+        console.log(directory)                                              //ICI BORDEL ERREUR ARRAY VIDE ALORS QU'IL DOIT RETURN LES PATHS POUR LES .TS FILES
+        const commandFiles = await globPromise(
+            `${directory}/*{.ts,.js}`
+        );
+        console.log(commandFiles)
+        commandFiles.forEach(async (filePath) => {
+            const command: CommandType = await this.importFile(filePath);
+            console.log(command)
+            if (!command.name) return;
+            console.log(command);
+
+            this.commands.set(command.name, command);
+            slashCommands.push(command);
+        });
+
+        this.on("ready", () => {
+            this.registerCommands({
+                commands: slashCommands,
+                guildId: process.env.guildId
+            });
+        });
+
+        // Event
+        const eventFiles = await globPromise(
+            `${process.cwd()}/../events/*{.ts,.js}`
+        );
+        eventFiles.forEach(async (filePath) => {
+            const event: Event<keyof ClientEvents> = await this.importFile(
+                filePath
+            );
+            this.on(event.event, event.run);
+        });
+    }
+}
