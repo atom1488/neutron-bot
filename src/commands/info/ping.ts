102a7215c1f40e24b7023b98d58487f3f4e116d7
atomkern <atomkern0@protonmail.com>
2022-06-16 20:38:15 +0200

    ajout de la commande ping

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,9 +1,9 @@
 import { Command } from "../../structures/Command";
 
 export default new Command({
-    name: "ping",
-    description: "replies with pong",
-    run: async ({ interaction }) => {
-        interaction.followUp("Pong3");
+    name: 'ping',
+    description: 'replies with bot\'s ping',
+    run: async({ client, interaction }) => {
+        interaction.followUp({ content: `The bot's ping is ${client.ws.ping}ms` });
     }
 });
