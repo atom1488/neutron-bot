18c7d8a7f902077372b817c2c0a795ee446c2bbb
atomkern <atomkern0@protonmail.com>
2022-06-16 22:53:49 +0200

    fetching all members at start

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,10 @@
 import { Event } from "../structures/Event";
+import {Guild} from "discord.js";
 
-export default new Event("ready", () => {
-    console.log("Bot is online");
+export default new Event("ready", (client) => {
+    client.guilds.cache.forEach(async (g: Guild) => {
+        await g.members.fetch();
+    });
+
+    console.log("Bot is online, all guilds members fetched!");
 });
