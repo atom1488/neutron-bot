cab58f139e2f5017693d4358877a39734911f8d1
atomkern <atomkern0@protonmail.com>
2022-06-18 12:59:05 +0200

    modification de la réponse ping.ts

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -4,6 +4,6 @@ export default new Command({
     name: 'ping',
     description: 'replies with bot\'s ping',
     run: async({ client, interaction }) => {
-        interaction.followUp({ content: `The bot's ping is ${client.ws.ping}ms` });
+        interaction.followUp({ content: `The bot's ping is \`${client.ws.ping}ms\`` });
     }
 });
