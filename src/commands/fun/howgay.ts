68b0e8199ee214d90e7e9f2783eebe904e5c61a8
Sorus <sorus@noreply.codeberg.org>
2022-06-18 16:17:52 +0200

    Ajout de la commande howgay

---
+++ src/commands/fun/howgay.ts
@@ -0,0 +1,18 @@
+import { Command } from "../../structures/Command";
+import { User } from "discord.js";
+
+export default new Command({
+    name: "howgay",
+    description: "Replies with gay %.",
+    options:[{
+        name: 'target',
+        type: 'USER',
+        description: 'Target user',
+        required: false
+    }],
+    run: async ({ interaction }) => {
+        const user: User = interaction.options.getUser('target') || interaction.user;
+        var randomNumber: number = Math.floor(Math.random() * 100) + 1;
+        interaction.followUp({content: `${user.username} is gay at ${randomNumber}% 🏳️‍🌈`})
+    }
+})
