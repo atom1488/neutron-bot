108b7a4dbed4129ad2bb2ad027be1acc52c3aa39
atomkern <atomkern0@protonmail.com>
2022-06-19 07:21:32 +0200

    environment example

--- web/strategies/discord.ts
+++ web/strategies/discord.ts
@@ -3,8 +3,8 @@ import passport from 'passport';
 import {VerifyCallback} from 'passport-oauth2';
 
 passport.use(new Strategy({
-    clientID: process.env.clientID,
-    clientSecret: process.env.clientSecret,
+    clientID: process.env.clientID as string,
+    clientSecret: process.env.clientSecret as string,
     callbackURL: process.env.callbackURL,
     scope: ['identify', 'email', 'guilds'],
 }, async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
