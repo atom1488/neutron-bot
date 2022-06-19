6c1028bf0ad63a0776adb8af94db34e38dfd92ea
atomkern <atomkern0@protonmail.com>
2022-06-19 06:43:32 +0200

    userInfo formatted

--- src/commands/fun/dice.ts
+++ src/commands/fun/dice.ts
@@ -3,14 +3,24 @@ import { MessageEmbed } from 'discord.js'
 
 export default new Command({
     name: 'dice',
-    description: 'Generate Number beetwen 1 and 100',
+    description: 'Generate a random number between 0 and a specefic value (default max value: 100)',
+    options: [{
+        name: 'value',
+        type: 'NUMBER',
+        description: 'The maximum value the dice can have',
+        required: false
+    }],
     run: async({ interaction }) => {
-        const randomNumber: number = Math.floor(Math.random() * 100) + 1;
+
+        const value: number = interaction.options.getNumber('value') || 100;
+
+        const randomNumber: number = Math.floor(Math.random() * value);
+
         const diceEmbed: MessageEmbed = new MessageEmbed()
             .setColor('#ee6f71')
             .setTitle('Dice')
             .setDescription(`:game_die: **${randomNumber}**`);
-        interaction.followUp({ embeds: [diceEmbed] });
 
+        interaction.followUp({ embeds: [diceEmbed] });
     }
 });
--- src/commands/info/userInfo.ts
+++ src/commands/info/userInfo.ts
@@ -1,51 +1,66 @@
 import { Command } from "../../structures/Command";
-import { CommandInteractionResolvedData, GuildMember, MemberMention, MessageEmbed, User } from "discord.js";
-import moment,{ Moment } from "moment";
+import { GuildMember, MessageEmbed } from "discord.js";
+import moment, { Moment } from "moment";
 export default new Command({
-    name: "userinfo",
-    description: "Replies user information.",
-    options: [{
-        name: "target",
-        type: "USER",
-        description: "Show user information",
-        required: false,
-    }],
-    run: async ({ interaction }) => {
-    const member: GuildMember = interaction.options.getMember("target") as GuildMember || interaction.member as GuildMember
+  name: "userinfo",
+  description: "Replies user information.",
+  options: [
+    {
+      name: "target",
+      type: "USER",
+      description: "Show user information",
+      required: false,
+    },
+  ],
+  run: async ({ interaction }) => {
+    const member: GuildMember =
+      (interaction.options.getMember("target") as GuildMember) ||
+      (interaction.member as GuildMember);
     const TimeFull: Moment = moment(member.user.createdAt);
     const TimeFullAt: Moment = moment(member.joinedAt);
-    const profilePicture: string = member.user.displayAvatarURL({ dynamic: true, size: 2048, format: "png" });
+    const profilePicture: string = member.user.displayAvatarURL({
+      dynamic: true,
+      size: 2048,
+      format: "png",
+    });
     const UserEmbed: MessageEmbed = new MessageEmbed()
-    .setAuthor({
+      .setAuthor({
         name: member.user.username,
-        iconURL: member.user.displayAvatarURL({ dynamic: true, size: 2048, format: "png" }),
-    })
-    .setColor("#ee6f71")
-    .setThumbnail(profilePicture)
-    .addFields(
+        iconURL: member.user.displayAvatarURL({
+          dynamic: true,
+          size: 2048,
+          format: "png",
+        }),
+      })
+      .setColor("#ee6f71")
+      .setThumbnail(profilePicture)
+      .addFields(
         {
-        name: "Name",
-        value: `<@${member.user.id}>`,
-        inline: true,
+          name: "Name",
+          value: `<@${member.user.id}>`,
+          inline: true,
         },
         {
-        name: "ID",
-        value: `${member.user.id}`,
-        inline: true,
+          name: "ID",
+          value: `${member.user.id}`,
+          inline: true,
         },
-        {"name": "\u200b", "value": "\u200b", inline: true},
+        { name: "\u200b", value: "\u200b", inline: true },
         {
-        name: "Created at",
-        value: `${TimeFull.format("dddd, MMMM Do YYYY, h:mm:ss a")} (<t:${Math.round(member.user.createdTimestamp / 1000)}:R>)`,
-        inline: false,
+          name: "Created at",
+          value: `${TimeFull.format(
+            "dddd, MMMM Do YYYY, h:mm:ss a"
+          )} (<t:${Math.round(member.user.createdTimestamp / 1000)}:R>)`,
+          inline: false,
         },
         {
-        name: "Joined at",
-        value: `${TimeFullAt.format("dddd, MMMM Do YYYY, h:mm:ss a")} (<t:${Math.round(member.joinedTimestamp / 1000)}:R>)`,
-        inline: false,
+          name: "Joined at",
+          value: `${TimeFullAt.format(
+            "dddd, MMMM Do YYYY, h:mm:ss a"
+          )} (<t:${Math.round(member.joinedTimestamp / 1000)}:R>)`,
+          inline: false,
         }
-    
-    )
+      );
     interaction.followUp({ embeds: [UserEmbed] });
-    }
+  },
 });
