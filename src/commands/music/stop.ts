85ab214dc153c1908db948bdaf67cd612102b9ed
atomkern <atomkern0@protonmail.com>
2022-07-05 03:56:12 +0200

    modif stop.ts

--- src/commands/music/stop.ts
+++ src/commands/music/stop.ts
@@ -1,20 +1,25 @@
-import { Queue } from 'discord-music-player';
-import { Command } from '../../structures/Command';
+import { Queue } from "discord-music-player";
+import { Command } from "../../structures/Command";
 
 export default new Command({
-    name: 'stop',
-    description: 'Stops the bot from playing music',
+    name: "stop",
+    description: "Stops the bot from playing music",
     run: async ({ interaction, client }) => {
-
-        if (!interaction.member.voice.channel) return interaction.followUp({ ephemeral: true, content: `You need to be in a voice channel to perform this command.` });
+        if (!interaction.member.voice.channel)
+            return interaction.followUp({
+                ephemeral: true,
+                content: `You need to be in a voice channel to perform this command.`
+            });
 
         if (interaction.guild.me.voice.channel) {
-            if (interaction.guild.me.voice.channelId != interaction.member.voice.channelId) return interaction.followUp({ ephemeral: true, content: `The bot is in an other voice channel.` });
+            if (interaction.guild.me.voice.channelId != interaction.member.voice.channelId)
+                return interaction.followUp({ ephemeral: true, content: `The bot is in an other voice channel.` });
         }
 
         const queue: Queue = client.player.getQueue(interaction.guildId);
 
-        if (!queue || !queue.isPlaying) return interaction.followUp({ ephemeral: true, content: `There is no music playing right now` });
+        if (!queue || !queue.isPlaying)
+            return interaction.followUp({ ephemeral: true, content: `There is no music playing right now` });
 
         queue.stop();
 
