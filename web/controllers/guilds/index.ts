de4e968dec6eaa9cd2e7c3293b35f20feee8e7e6
atomkern <atomkern0@protonmail.com>
2022-07-06 04:26:54 +0200

    adding web: nextJS (wip)

--- web/controllers/guilds/index.ts
+++ web/controllers/guilds/index.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
-import { getBotGuildsService } from '../../services/guilds';
+import { User } from '../../database/schemas/User';
+import { getMutualGuildsService } from '../../services/guilds';
 
 export async function getGuildsController(req: Request, res: Response) {
+  const user = req.user as User;
   try {
-    const { data } = await getBotGuildsService();
-    res.send(data);
+    const guilds = await getMutualGuildsService(user.id);
+    res.send({ guilds });
   } catch (err) {
     console.error(err);
     res.sendStatus(400).send('Error');
   }
-  res.sendStatus(200);
 }
--- web/services/guilds/index.ts
+++ web/services/guilds/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { User } from '../../database/schemas/';
 import { DISCORD_API_URL } from '../../utils/constants';
 import { PartialGuild } from '../../utils/types';
 
@@ -7,4 +8,21 @@ export function getBotGuildsService() {
   return axios.get<PartialGuild[]>(`${DISCORD_API_URL}/users/@me/guilds`, {
     headers: { Authorization: `Bot ${TOKEN}` },
   });
+}
+
+export async function getUserGuildsService(id: string) {
+  const user = await User.findById(id);
+  if (!user) throw new Error('No user found');
+  return axios.get<PartialGuild[]>(`${DISCORD_API_URL}/users/@me/guilds`, {
+    headers: { Authorization: `Bearer ${user.accessToken}` },
+  });
+}
+
+export async function getMutualGuildsService(id: string) {
+  const { data: botGuilds } = await getBotGuildsService();
+  const { data: userGuilds } = await getUserGuildsService(id);
+
+  const adminUserGuilds = userGuilds.filter(({ permissions }) => (parseInt(permissions) & 0x8) === 0x8);
+
+  return adminUserGuilds.filter((guild) => botGuilds.some((botGuild) => botGuild.id === guild.id));
 }
