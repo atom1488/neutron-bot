f04fbe66a5ecbe2288baa1b39a3dd365733b5de1
atomkern <atomkern0@protonmail.com>
2022-07-09 05:55:09 +0200

    adding /invite endpoint

--- web/neutron-dashboard-client/src/middleware.ts
+++ web/neutron-dashboard-client/src/middleware.ts
@@ -1,5 +1,9 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import { fetchValidGuild } from './utils/api';
+
+const DISCORD_INVITE_URL =
+  'https://discord.com/oauth2/authorize?response_type=code&scope=identify%20email%20guilds&client_id=986815196376989756';
+
 const validateMiddlewareCookies = (req: NextRequest) => {
   const sessionID = req.cookies.get('connect.sid');
   return sessionID
@@ -25,7 +29,11 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
       return (url.pathname = '/'), NextResponse.redirect(url);
     }
   }
+
+  if (req.nextUrl.pathname.startsWith('/invite')) {
+    return NextResponse.redirect(DISCORD_INVITE_URL);
+  }
 }
 export const config = {
-  matcher: '/dashboard/:path*',
+  matcher: ['/dashboard/:path*', '/invite'],
 };
