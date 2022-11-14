8afc4e83e561ce40ef28abb422659d334960472e
atom1488 <atom1488@gpa.lu>
2022-11-14 19:15:07 +0100

    oups

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -3,7 +3,6 @@ import { Command } from '../../structures/Command';
 export default new Command({
   name: 'ping',
   description: "replies with bot's ping",
-  userPermissions: ['Administrator'],
   run: async ({ client, interaction }) => {
     interaction.followUp({ content: `The bot's ping is \`${client.ws.ping}ms\`` });
   },
